286d108440604c2ebe4e2e150e9b50ad7e586393
isaax2 <isaacisaacisaac2@gmail.com>
2020-01-23 14:28:41 -0600

    Secondary Routes

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 import {AuthService} from './user/auth.service';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent} from '@angular/router';
 import {slideInAnimation} from './app.animation';
+import {MessageService} from './messages/message.service';
 
 @Component({
   selector: 'pm-root',
@@ -18,6 +19,10 @@ export class AppComponent {
     return this.authService.isLoggedIn;
   }
 
+  get isMessageDisplayed(): boolean {
+    return this.messageService.isDisplayed;
+  }
+
   get userName(): string {
     if (this.authService.currentUser) {
       return this.authService.currentUser.userName;
@@ -26,7 +31,8 @@ export class AppComponent {
   }
 
   constructor(private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private messageService: MessageService) {
 
     this.router.events.subscribe((routerEvent: RouterEvent) => {
       this.checkRouterEvent(routerEvent);
@@ -45,9 +51,25 @@ export class AppComponent {
     }
   }
 
+  displayMessages(): void {
+    this.router.navigate([{
+      outlets: {
+        popup: ['messages']
+      }
+    }]);
+    this.messageService.isDisplayed = true;
+  }
+
+  hideMessages(): void {
+    this.router.navigate([{outlets: {popup: null}}]);
+    this.messageService.isDisplayed = false;
+  }
+
   logOut(): void {
     this.authService.logout();
     console.log('Log out');
     this.router.navigateByUrl('/welcome');
   }
+
+
 }
