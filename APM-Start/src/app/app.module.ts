376b6e2ed403b0e3f196ec29ff1a274649134d58
isaax2 <isaacisaacisaac2@gmail.com>
2020-01-24 12:49:54 -0600

    Lazy Loading

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -11,7 +11,7 @@ import {WelcomeComponent} from './home/welcome.component';
 import {PageNotFoundComponent} from './page-not-found.component';
 
 /* Feature Modules */
-import {ProductModule} from './products/product.module';
+
 import {UserModule} from './user/user.module';
 import {MessageModule} from './messages/message.module';
 import {AppRoutingModule} from './app-routing.module';
@@ -23,7 +23,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, {delay: 1000}),
-    ProductModule,
+
     UserModule,
     MessageModule,
     AppRoutingModule
